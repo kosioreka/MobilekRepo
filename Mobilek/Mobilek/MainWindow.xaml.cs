8400f2bcc68bbc6bbe0f58da4984134530d030e6
mkwie <matkwiecinski@gmail.com>
2015-10-28 19:45:03 +0100

    Project Added
    DB created

---
+++ Mobilek/Mobilek/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mobilek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataBaseTest();
+        }
+        public void DataBaseTest()
+        {
+            MobilekEntities entities = new MobilekEntities();
+
+            Station newStation = new Station();
+            newStation.city = "Wwa";
+            newStation.street = "Powsinska";
+            newStation.streetNumer = 40;
+            newStation.zipCode = "0,7";
+
+            entities.Stations.Add(newStation);
+
+            entities.SaveChanges();
+        }
+    }
+}
